3e17a8268e333662dd61290b92dbd85cf7e91d47
Casey Hudler <chudler624@gmail.com>
2022-08-12 16:12:36 -0400

    lukeWarmComplete

---
+++ IterationsExercise3/Program.cs
@@ -0,0 +1,53 @@
+﻿//1.  print all #'s from -1000 to 1000
+
+//for (int i = -1000; i <= 1000; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+////2.print all #'s 3-999 by 3's
+//for (int i = 3; i <= 999; i+=3)
+//{
+//   Console.WriteLine(i);
+//}
+
+//3.Accept 2 parameters, see if they are equal
+//Console.WriteLine("Enter variable A");
+//var a= Console.ReadLine();
+//Console.WriteLine("Enter variable B");
+//var b= Console.ReadLine();
+
+//if (a == b)
+//{ Console.WriteLine($"{a} Equals {b}"); }
+//else
+//{ Console.WriteLine($"{a} does not equal {b}"); }
+
+//4. IS given number even or odd
+
+//Console.WriteLine("Enter a Number");
+//    var num= int.Parse(Console.ReadLine());
+
+//if (num%2==0)
+//{ Console.WriteLine($"{num} is even"); }
+//else
+//{
+//    Console.WriteLine($"{num} is odd");
+//}
+
+//5. Is given number positive or negative
+//Console.WriteLine("Write a number");
+//var number = int.Parse(Console.ReadLine());
+//if (number > 0)
+//{ Console.WriteLine($"{number} is Positive"); }
+//if (number < 0)
+//{ Console.WriteLine($"{number} is Negative"); }
+//if (number ==0)
+//  Console.WriteLine($"{number} is neither positive or negative");
+
+////6. Read the age of person to see if they can vote
+//Console.WriteLine("Enter your age");
+//var age = int.Parse(Console.ReadLine());
+//if (age >= 18)
+//    Console.WriteLine("Make sure you go vote");
+//if (age <=17)
+//    Console.WriteLine("Pretty soon you'll be able to vote");
